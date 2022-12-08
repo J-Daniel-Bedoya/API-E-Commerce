c18f3d4dac65b42f1e96ca9d39c5127a4b868fa2
daniel1718 <jbedoyachavarriaga@gmail.com>
2022-12-08 07:38:10 -0500

    change total

--- src/services/cart.services.js
+++ src/services/cart.services.js
@@ -83,13 +83,13 @@ class CartServices {
         if (Number(idProduct) === pro.dataValues.productId){
           pro.destroy()
         }
+        const totalPriceCartArray = await ProductInCart.findAll();
+        const totalPriceCart = totalPriceCartArray.map(total => { return total.price});
+        const priceTotal = totalPriceCart.reduce((a, b) => a + b);
+        const cart = await Cart.findOne({where: {id: idCart}});
+        await cart.update({totalPrice: priceTotal});
       })
       
-      const totalPriceCartArray = await ProductInCart.findAll();
-      const totalPriceCart = totalPriceCartArray.map(total => { return total.price});
-      const priceTotal = totalPriceCart.reduce((a, b) => a + b);
-      const cart = await Cart.findOne({where: {id: idCart}});
-      await cart.update({totalPrice: priceTotal});
 
       return prod;
     } catch (error) {
